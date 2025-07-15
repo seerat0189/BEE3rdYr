26e6901103a80be95bc7884cabc896c50d2f054e
seerat0189 <149876947+seerat0189@users.noreply.github.com>
2025-07-15 09:31:01 +0530

    Add files via upload
    lec5

---
+++ lec5/notes.js
@@ -0,0 +1,6 @@
+/* 14-07-2025
+    promise chaining
+    async await
+    file handling: it is an i/o operation
+    node.js provides 3 methods for file handling: synchronous, asynchronous (callback and promise)
+*/
