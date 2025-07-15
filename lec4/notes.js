470a43fd6a011168f02ca301b773cfae60946034
seerat0189 <149876947+seerat0189@users.noreply.github.com>
2025-07-15 09:30:31 +0530

    Add files via upload
    lec4

---
+++ lec4/notes.js
@@ -0,0 +1,8 @@
+/* 08-07-2025
+callbacks and promises (to handle asynchronous programs)
+promise is an object which represents eventual completion or failure of an asynchronous operation
+states in promise are pending, fulfilled and rejected
+if state is fulfilled, .then works
+if state is rejected, .catch works
+inversion control
+*/
